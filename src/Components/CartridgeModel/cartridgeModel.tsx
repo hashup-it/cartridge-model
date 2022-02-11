dfd6e733586092b69578959296cc75e08093dd49
damiansarnecki <xdamiansarnecki@gmail.com>
2022-02-11 07:00:15 +0100

    added all needed props

--- src/Components/CartridgeModel/cartridgeModel.tsx
+++ src/Components/CartridgeModel/cartridgeModel.tsx
@@ -3,19 +3,21 @@ import { FC, useEffect, useState } from 'react';
 interface Props {
 	color: string;
 	image: string;
-	fov: number
+	fov: number,
+	width: string,
+	height: string
 }
 
 
-export const CartridgeModel: FC<Props> = ({ color, image, fov}) => {
-	const [framePath, setFramePath] = useState<string>(`${window.location.protocol}//${window.location.host}/cartridgeModel/`);
+export const CartridgeModel: FC<Props> = ({ color, image, fov, width, height}) => {
+	const [framePath, setFramePath] = useState<string>(``);
 
 	useEffect(() => {
-		const path = `${window.location.protocol}//${window.location.host}/cartridgeModel/?qsfov=40&qscolor=${color}&qsimage=${image}`;
+		const path = `${window.location.protocol}//${window.location.host}/cartridgeModel/?qsfov=${fov}&qscolor=${color}&qsimage=${image}`;
 		setFramePath(path);
-	}, [color, image]);
+	}, [color, image, fov]);
 
 	return (
-		<iframe title="cartridge model" src={framePath} width="400px" height="300px" />
+		<iframe title="cartridge model" src={framePath} width={width} height={height} />
 	);
 };
