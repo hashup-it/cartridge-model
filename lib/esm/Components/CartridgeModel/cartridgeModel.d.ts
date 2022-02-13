80cbef77006873c9a5dc23f1387de2dfe69a0b6c
badrat <mczarkowski@hashup.it>
2022-02-13 20:34:17 +0100

    new build

---
+++ lib/esm/Components/CartridgeModel/cartridgeModel.d.ts
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+interface Props {
+    color: string;
+    image: string;
+    fov: number;
+    width: string;
+    height: string;
+}
+export declare const CartridgeModel: FC<Props>;
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {CartridgeModel} from "./Components/CartridgeModel/cartridgeModel";
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default CartridgeModel
 
