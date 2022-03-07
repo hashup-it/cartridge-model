d0eecf40c77cc40a48442c78c8c581f8763e149f
Bartlomiej Zając <bartlomiejzajac@bartlomiejs-macbook-pro.home>
2022-03-08 00:44:16 +0100

    disable preload

---
+++ public/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1295548.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': false,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 63270377, 63270389, 63270388, 63270386, 63270385, 63270376, 63270407 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/63745236/1/basis.wasm.js', 'wasmUrl' : 'files/assets/63745237/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/63745235/1/basis.js', 'preload' : false},
+];
