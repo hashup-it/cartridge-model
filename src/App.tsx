edb20a1a9ba4eb23e9213abdb96d33b66ab400c7
Robert Dziubek <rdziubek@hashup.it>
2022-06-14 10:51:24 +0200

    Test cartridge impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
 import './App.css';
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, CameraShake, Environment } from '@react-three/drei';
-import Model from './Components/Models/Model';
+import React, { Suspense, useMemo, useState } from 'react';
+import { Canvas, extend } from '@react-three/fiber';
+import { CameraShake, Environment, OrbitControls, TrackballControls } from '@react-three/drei';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import CartridgeModel from './Components/Models/CartridgeModel';
+import { EffectComposer } from '@react-three/postprocessing';
+
+export enum Cartridge {
+    GOLD,
+    SILVER,
+    RAINBOW,
+    _ /** Null cartridge */
+}
+
+const hdrMaps: { [p: number]: string } = {
+    [Cartridge.GOLD]: '/assets/models/zloty/zloty.hdr',
+    [Cartridge.SILVER]: '/assets/models/szary/szary.hdr',
+    [Cartridge.RAINBOW]: '/assets/models/teczowy/teczowy.hdr',
+    [Cartridge._]: '/assets/models/pusty/pusty.hdr'
+};
+
+extend({ RenderPass });
 
 function App() {
-	return (
-		<div className="canvas-container">
-			<Canvas camera={{position: [-200, 0, 150], fov: 30}}>
-				<Suspense fallback={null}>
-					<Model />
-					<Environment preset="sunset" background />
+    /** TODO: Expose `setCartridge` */
+    const [cartridge, setCartridge] = useState(Cartridge._);
 
-					<OrbitControls makeDefault autoRotate autoRotateSpeed={1} />
-					<CameraShake maxRoll={0.05} maxPitch={0.05} maxYaw={0.05} />
-				</Suspense>
-			</Canvas>
-		</div>
-	);
+    const isLoading = useMemo(() => cartridge === Cartridge._, [cartridge]);
+
+    return (
+        <div className="canvas-container" style={{ background: 'transparent', position: 'relative' }}>
+            <div style={{ color: 'white', position: 'absolute', top: '0', left: '0', zIndex: '1' }}>_set cartridge
+                <div onClick={() => setCartridge(Cartridge.GOLD)}>gold</div>
+                <div onClick={() => setCartridge(Cartridge.RAINBOW)}>rainbow</div>
+                <div onClick={() => setCartridge(Cartridge.SILVER)}>silver</div>
+                <div onClick={() => setCartridge(Cartridge._)}>null</div>
+            </div>
+
+            <Canvas camera={{ position: [1.5, 0.5, 1], fov: 60 }}>
+                <Suspense fallback={null}>
+                    <CartridgeModel cartridge={cartridge} />
+                    <Environment files={hdrMaps[cartridge]} />
+                    {!isLoading || <ambientLight />}
+
+                    <OrbitControls makeDefault autoRotate autoRotateSpeed={1} enablePan={false} enableZoom={false} />
+                    {/** Zoom control */}
+                    <TrackballControls noRotate={true} noPan={true} noZoom={false} zoomSpeed={1.5} />
+                    <CameraShake maxRoll={0.05} maxPitch={0.05} maxYaw={0.05} />
+
+                    <EffectComposer>
+                        {/*<SSAO />*/}
+                        {/*<Bloom luminanceThreshold={.5} kernelSize={5}/>*/}
+                    </EffectComposer>
+                </Suspense>
+            </Canvas>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Models/Box.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Box = (props: JSX.IntrinsicElements['mesh']) => (
-	<mesh
-		{...props}>
-		<boxGeometry args={[1, 1, 1]} />
-		<meshStandardMaterial color={'hotpink'} />
-	</mesh>
-);
